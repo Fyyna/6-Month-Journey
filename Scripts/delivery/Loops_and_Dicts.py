2052fdae69d4672a24dada94ae43a553aa8b950f
Fyyna <dominherzog@gmail.com>
2026-02-07 20:35:17 +0100

    added comments to script to understand and for practice

--- Scripts/delivery/Loops_and_Dicts.py
+++ Scripts/delivery/Loops_and_Dicts.py
@@ -1,3 +1,4 @@
+#temporary dict data for calculations :P no CSV or JSON Parsin yet
 delivery_data = [
         {"driver_id": "D1", "pickup_time": "09:00", "dropoff_time": "09:30", "distance_km": 10},
         {"driver_id": "D2", "pickup_time": "09:15", "dropoff_time": "10:00", "distance_km": 20},
@@ -6,13 +7,14 @@ delivery_data = [
         {"driver_id": "D2", "pickup_time": "10:30", "dropoff_time": "11:30", "distance_km": 25},
     ]
 
+#determines slowest Driver
 slowest = None
-slowest_driver = 0
+slowest_driver = None
 
 
 
 
-
+#function to natively convert HH:MM form a string to an integers in Minutes
 def conversion_to_minutes(time_str):
 
     parts = time_str.split(":")
@@ -23,15 +25,19 @@ def conversion_to_minutes(time_str):
 
     return time
 
+#Empty Driver Stats dict
 driver_stats = {}
 
-
-#print (conversion_to_minutes(delivery_data[0]["pickup_time"]))
-
+#empty slow driver list
 slow_deliveries = []
+
+#core Loop
 for d in delivery_data:
 
+    #assigns dict entry "driver_id" to variable driver_id to be usable
     driver_id = d["driver_id"]
+
+    #checks if driver id is already in driver_stats dict if it isnt it creates a new entry in the dict with the schematic dict provided
     if driver_id not in driver_stats:
        driver_stats[driver_id] = {
            "total_distance_km": 0,
@@ -39,17 +45,27 @@ for d in delivery_data:
            "total_deliveries": 0
        }
 
-
-
+    # extract dict entry for readability and avoiding repeated indexing
     pickup = d["pickup_time"]
     dropoff = d["dropoff_time"]
+
+    #calculates the time it took to deliver with the function conversion_to_minutes using the variables wew created
     delivery_time_minutes = conversion_to_minutes(dropoff) - conversion_to_minutes(pickup)
+
+    #converts the delivery time into hours for speed calculation
     delivery_time_hours = delivery_time_minutes / 60
+
+    #takes the distance we traveled and divides it by time in hours for KM/H
     speed = d["distance_km"] / delivery_time_hours
 
+    #assigns a driver_id to the slow list if it is slower than 20 KM/H
     if speed < 20:
         slow_deliveries.append(d)
+
+        #saves speed to dict entry "speed"
         d["speed"] = speed
+
+        #saves delivery_time_minutes to dict entry "delivery_time_minutes"
         d["delivery_time_minutes"] = delivery_time_minutes
 
 
@@ -59,26 +75,30 @@ for d in delivery_data:
 
     print(f"The delivery took {delivery_time_minutes} minutes and was delivered at an average speed of {speed: .2f} Km/h")
 
-
+#loop to return Key (driver ID) and values (driver_stats.items()) calculations deliberatly done after we gotten all data to not average averages
 for driver_id, stats in driver_stats.items():
 
-   total_distance_km = stats["total_distance_km"]
+    # extract dict entry for readability and avoiding repeated indexing
+    total_distance_km = stats["total_distance_km"]
 
-   total_time_minutes = stats["total_time_minutes"]
+    # extract dict entry for readability and avoiding repeated indexing
+    total_time_minutes = stats["total_time_minutes"]
 
-   total_deliveries = stats["total_deliveries"]
+    # extract dict entry for readability and avoiding repeated indexing
+    total_deliveries = stats["total_deliveries"]
 
-   average_distance = total_distance_km / total_deliveries
+    #calculates the average distance based on total distance divided by total ammount of deliveries
+    average_distance = total_distance_km / total_deliveries
 
-   average_speed = total_distance_km / (total_time_minutes / 60)
+    #calculates average speed based on total distance and minutes / 60 to get KM/H
+    average_speed = total_distance_km / (total_time_minutes / 60)
 
-   if slowest is None or average_speed < slowest:
+
+    if slowest is None or average_speed < slowest:
        slowest = average_speed
        slowest_driver = driver_id
 
 
-   print(f"Driver ID: {driver_id}, Average Distance: {average_distance}, Average Speed:{average_speed: .2f}")
-
-
+    print(f"Driver ID: {driver_id}, Average Distance: {average_distance} km, Average Speed:{average_speed: .2f} KM/H")
 
 print (f"slowest driver was: {slowest_driver} fire this bitch")
